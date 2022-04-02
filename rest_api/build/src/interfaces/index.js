8f05cc5baf820e8c7512b78a26e8a64ef4e00fa0
greensoap <william.sjokvist@gmail.com>
2022-04-02 21:37:36 +0200

    gitignored build folder

--- rest_api/build/src/interfaces/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.OrganizationObjectError = exports.validateOrganizationObject = void 0;
-const organization_object_1 = require("./organization-object");
-Object.defineProperty(exports, "validateOrganizationObject", { enumerable: true, get: function () { return organization_object_1.validateOrganizationObject; } });
-Object.defineProperty(exports, "OrganizationObjectError", { enumerable: true, get: function () { return organization_object_1.OrganizationObjectError; } });
